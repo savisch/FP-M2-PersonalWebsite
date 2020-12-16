60b2ea0aaaa997a0c6635971e2e9c1c66f0b546e
savisch <savisch@gmail.com>
2020-12-16 12:31:15 -0500

    Link out pinned repo names to Github

--- script.js
+++ script.js
@@ -8,26 +8,33 @@ newRequest.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200){
         let repoObj = JSON.parse(this.responseText);
         document.getElementById("n1").textContent = repoObj[0].repo;
+        document.getElementById("n1").href = repoObj[0].link;
         document.getElementById("d1").textContent = repoObj[0].description;
         document.getElementById("l1").textContent = repoObj[0].language;
 
+
         document.getElementById("n2").textContent = repoObj[1].repo;
+        document.getElementById("n2").href = repoObj[1].link;
         document.getElementById("d2").textContent = repoObj[1].description;
         document.getElementById("l2").textContent = repoObj[1].language;
 
         document.getElementById("n3").textContent = repoObj[2].repo;
+        document.getElementById("n3").href = repoObj[2].link;
         document.getElementById("d3").textContent = repoObj[2].description;
         document.getElementById("l3").textContent = repoObj[2].language;
 
         document.getElementById("n4").textContent = repoObj[3].repo;
+        document.getElementById("n4").href = repoObj[3].link;
         document.getElementById("d4").textContent = repoObj[3].description;
         document.getElementById("l4").textContent = repoObj[3].language;
 
         document.getElementById("n5").textContent = repoObj[4].repo;
+        document.getElementById("n5").href = repoObj[4].link;
         document.getElementById("d5").textContent = repoObj[4].description;
         document.getElementById("l5").textContent = repoObj[4].language;
 
         document.getElementById("n6").textContent = repoObj[5].repo;
+        document.getElementById("n6").href = repoObj[5].link;
         document.getElementById("d6").textContent = repoObj[5].description;
         document.getElementById("l6").textContent = repoObj[5].language;
     };
