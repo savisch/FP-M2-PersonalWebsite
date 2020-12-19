d9e415932acb06dd8bc7d5c1c02e74c79866a2ce
savisch <swsolns@gmail.com>
2020-12-19 10:35:27 -0600

    Give footer icons jQuery animation

--- script.js
+++ script.js
@@ -6,10 +6,17 @@
 // Animate social icons on hover with jQuery
 $(document).ready(function(){
     $(".social-icon").hover(function(){
-        $(this).css({"box-shadow":"inset 0 0 0 50vw rgb(235, 235, 235)", "border-radius":"12px", "width":"80%"});
+        $(this).css({"box-shadow":"inset 0 0 0 50vw rgb(235, 235, 235)", "border":"1px solid black", "border-radius":"12px", "width":"80%"});
     },
     function(){
-        $(this).css({"box-shadow":"none", "border-radius":"none", "width":""});
+        $(this).css({"box-shadow":"none", "border":"none", "border-radius":"none", "width":""});
+    });
+
+    $(".footer-icon").hover(function(){
+        $(this).css({"box-shadow":"inset 0 0 0 50vw rgb(195, 195, 195)", "border":"1px solid black", "border-radius":"12px", "width":"13%"});
+    },
+    function(){
+        $(this).css({"box-shadow":"none", "border":"none", "border-radius":"none", "width":""});
     });
 });
 
